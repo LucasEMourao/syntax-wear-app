4228a0eb2fb1ee0976f7ee3b1cfec10a73718aa0
LucasEMourao <lucasemourao2@gmail.com>
2025-12-18 10:15:12 -0300

    feat:adiciona componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Header } from "./components/Header"
+
 function App() {
-  return <>
-    <p className="text-error">Hello World</p>
-  </>;
+
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
 
-export default App;
+export default App
