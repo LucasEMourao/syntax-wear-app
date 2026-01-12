6b9c44ff44a69e410e67a6e176b5fa48696243f5
LucasEMourao <lucasemourao2@gmail.com>
2026-01-12 08:31:36 -0300

    feat:adiciona tela nossas lojas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ import { ShoppingCart } from "../ShoppingCart";
 export const Header = () => {
   return (
     <div className="relative">
-      <header className="fixed top-10 left-0 right-0 z-10 mx-10">
+      <header className="fixed top-5 left-0 right-0 z-10 mx-10">
         <div className="bg-white text-black max-w-330 mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5">
           <Link to="/">
             <img src={Logo} alt="Logo SyntaxWear" className="w-32 md:w-36" />
@@ -16,7 +16,7 @@ export const Header = () => {
           <nav className="hidden md:block">
             <ul className="flex gap-10">
               <li>
-                <a href="#">Masculino</a>
+                <Link to="/our-stores">Masculino</Link>
               </li>
               <li>
                 <a href="#">Feminino</a>
