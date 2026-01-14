be846347316ec7cd352f1c322feaa05793631feb
LucasEMourao <lucasemourao2@gmail.com>
2026-01-14 17:07:00 -0300

    feat: criados componentes da drawer e do botão de carrinho

--- src/components/CartDrawer/index.tsx
+++ src/components/CartDrawer/index.tsx
@@ -1,49 +1,41 @@
-import { useContext, useState } from "react";
-import IconCart from "../../assets/images/icon-cart.png";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/CartContext";
 import { formatCurrency } from "../../utils/format-currency";
 
-import { CartContext } from "../../contexts/CartContext";
+interface CartDrawerProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
 
-export const ShoppingCart = () => {
-  const [cartIsOpen, setCartIsOpen] = useState<boolean>(false);
-  const { cart, remove, increment, decrement } = useContext(CartContext);
+export const CartDrawer = ({ isOpen, onClose }: CartDrawerProps) => {
+  const { cart, remove, increment, decrement } =
+    useContext(CartContext);
 
   return (
     <>
-      <button
-        className="relative cursor-pointer"
-        onClick={() => setCartIsOpen(!cartIsOpen)}
-      >
-        <img src={IconCart} alt="icone do carrinho de compras" />
-        {cart.length > 0 && (
-          <span className="absolute -top-3 -right-2 bg-error text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
-            {cart.length}
-          </span>
-        )}
-      </button>
-
+      {/* Overlay */}
       <div
-        className={`${cartIsOpen ? "bg-black/70 visible" : "bg-transparent invisible"} fixed top-0 bottom-0 left-0 right-0`}
-        onClick={() => setCartIsOpen(!cartIsOpen)}
+        className={`${isOpen ? "bg-black/70 visible" : "bg-transparent invisible"} text-black fixed inset-0 z-50 transition-all duration-600 ease-in-out`}
+        onClick={onClose}
       >
+        {/* Drawer
+            translate-x-0 - posição normal da drawer
+            translate-x-full - fora da tela (para a direita)
+        */}
         <div
-          className={`${cartIsOpen ? "translate-x" : "translate-x-full"} absolute top-0 right-0 bottom-0 bg-white pt-6 transition-all duration-500 ease-in-out w-75 md:w-100`}
+          className={`${isOpen ? "translate-x-0" : "translate-x-full"} absolute top-0 right-0 bottom-0 bg-white pt-6 transition-all duration-500 ease-in-out w-75 md:w-100`}
           onClick={(e) => e.stopPropagation()}
         >
           <header className="flex items-center justify-between px-5">
-            <p className="text-2xl font-bold"> Carrinho ({cart.length})</p>
-            <button
-              className="text-xl cursor-pointer"
-              onClick={() => setCartIsOpen(!cartIsOpen)}
-            >
+            <p className="text-2xl font-bold">Carrinho ({cart.length})</p>
+            <button className="text-xl cursor-pointer" onClick={onClose}>
               X
             </button>
           </header>
 
-          <ul className="p-4 overflow-y-auto scrollbar-hide h-[calc(100%_-_140px)] flex flex-col gap-12">
+          <ul className="p-4 overflow-y-auto scrollbar-hide h-[calc(100%_-_140px)] flex flex-col gap-3">
             {cart.map((product) => (
-              <li key={product.id} className="flex flex-col gap-1 px-6">
-                {product.name}
+              <li key={product.id} className="flex flex-col gap-1 pr-2">
                 <button
                   className="self-end text-xs cursor-pointer"
                   onClick={() => remove(product.id)}
@@ -55,7 +47,7 @@ export const ShoppingCart = () => {
                   <img
                     src={product.image}
                     alt={product.name}
-                    className="w-16 h-16"
+                    className="w-24 h-24 md:w-32 md:h-32"
                   />
 
                   <div className="flex flex-col items-start">
@@ -63,11 +55,14 @@ export const ShoppingCart = () => {
                     <p className="mb-1 text-sm">
                       Quantidade: {product.quantity}
                     </p>
+
                     <p className="mb-3.5">
                       <span className="font-bold mr-1.5">
-                        {formatCurrency(product.price)} à vista
+                        {formatCurrency(product.price)}
                       </span>{" "}
+                      à vista
                     </p>
+
                     <div className="border flex gap-6 py-1 px-3">
                       <button
                         className="cursor-pointer"
@@ -91,7 +86,7 @@ export const ShoppingCart = () => {
 
           <footer className="absolute bottom-0 w-full h-[100px] p-4">
             <button className="w-full h-full bg-black text-white rounded-xs cursor-pointer hover:bg-gray-800">
-              Fechar Pedido
+              Fechar pedido
             </button>
           </footer>
         </div>
